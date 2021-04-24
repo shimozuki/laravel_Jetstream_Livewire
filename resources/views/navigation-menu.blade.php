c6149901dca74daf8cc0653aac0a7dc946082623
robbishimozuki <robbishimozuki@gmail.com>
2021-04-24 22:52:31 +0800

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                     <x-jet-nav-link href="{{ route('ujian') }}" :active="request()->routeIs('ujian')">
+                        {{ __('List Ujian') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
